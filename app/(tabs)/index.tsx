a278bf1093feee93e85fd59f1812aa3da92290a0
Moahmed Almarty <moalmatry2000@gmail.com>
2025-03-09 20:12:03 +0200

    feat: firist init

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, View, Text } from "react-native";
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.titleContainer}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
